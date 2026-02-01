0903edfc4c734b8f72e6335d1f7ecf7f87aa5ecd
Sandesh <sandeshchy2059@gmail.com>
2026-02-01 20:27:23 +0545

    added clicks in model

--- URLshortener/shortener/models.py
+++ URLshortener/shortener/models.py
@@ -2,11 +2,13 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class UrlShortener(models.Model):
-    user_name = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     original_url = models.URLField()
     short_url = models.CharField(max_length=10, unique=True)
+    clicks = models.PositiveIntegerField(default=0)
 
     def __str__(self):
         return self.original_url
+    
 
 
