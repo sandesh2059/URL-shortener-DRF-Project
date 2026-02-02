af4a50ef8993d4a3bd8f125ea0494fe2d7c97ea1
Sandesh <sandeshchy2059@gmail.com>
2026-02-02 21:39:26 +0545

    fields and read only fields updated

--- URLshortener/shortener/serializers.py
+++ URLshortener/shortener/serializers.py
@@ -5,8 +5,12 @@ class LongUrlSerializer(serializers.ModelSerializer):
     short_url = serializers.CharField(required=False, allow_blank=True) 
     class Meta:
         model = UrlShortener
-        fields = ['user','original_url', 'short_url', 'created_at', 'clicks']
-        read_only_fields = ['created_at', 'clicks']
+        fields = ['id', 'user','original_url', 'short_url', 'created_at', 'clicks']
+        read_only_fields = ['id','user', 'created_at', 'clicks']
+    
+    def create(self, validated_data):
+        validated_data['user'] = self.context['request'].user
+        return super().create(validated_data)
 
 
 
