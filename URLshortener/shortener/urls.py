2e742f3720868d73b654cb0ac758a1a3dd297110
Sandesh <sandeshchy2059@gmail.com>
2026-02-02 21:38:18 +0545

    url path for url-dashboard created

--- URLshortener/shortener/urls.py
+++ URLshortener/shortener/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from .views import LongUrlApiView, longurl_list, RedirectUrl
+from .views import LongUrlApiView, longurl_list, RedirectUrl, url_dashboard
 from . import views
 
 urlpatterns = [
+    path('url-dashboard/', views.url_dashboard, name = 'url-dashboard'),
     path('longurl/', views.LongUrlApiView.as_view(), name='longurl'),
     path('longurl-list/', views.longurl_list, name='longurl-list'),
     path('<str:short_url>/', views.RedirectUrl.as_view(), name='redirect-url'),
     path('longurl/<str:short_url>/', views.LongUrlApiView.as_view(), name='longurldetail'),
+    
 ]
